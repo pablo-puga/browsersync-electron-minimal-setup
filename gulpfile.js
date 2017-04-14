6b0daafad187aa326dcc5cd12a0a39606142de96
Pablo Puga <pablo.puga.git@gmail.com>
2017-04-14 03:32:20 +0200

    Added the logic to package the Electron app

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const plumber = require('gulp-plumber');
 const runElectron = require('gulp-run-electron');
+const replace = require('gulp-replace');
+const del = require('del');
 
 gulp.task('default', ['watchers', 'run-electron']);
 
@@ -25,4 +27,35 @@ gulp.task('watchers', function () {
 
 gulp.task('run-electron', function () {
     gulp.src('.').pipe(runElectron([], {}));
+});
+
+gulp.task('build-app', function (done) {
+    const builder = require('electron-builder');
+
+    gulp.src('app/index.html').pipe(gulp.dest('dist/tmp-files'));
+
+    gulp.src('app/index.html')
+        .pipe(replace('<script async id="__bs_script__" src="http://localhost:3000/browser-sync/browser-sync-client.js?v=2.18.8"></script>',''))
+        .pipe(gulp.dest('app/'));
+
+    builder.build({
+        config: {
+            appId: 'com.electron.template',
+            productName: 'BrowserSyncElectronMinimalSetup',
+            buildVersion: '1.0.0',
+            copyright: 'Pablo Puga',
+            artifactName: '${productName}_${version}.${ext}'
+        }
+    }).then(function () {
+        closeTask();
+    }).catch(function (error) {
+        console.error('Build Error', error);
+        closeTask();
+    });
+
+    function closeTask() {
+        gulp.src('dist/tmp-files/index.html').pipe(gulp.dest('app/'));
+        del(['dist/tmp-files/']);
+        done();
+    }
 });
